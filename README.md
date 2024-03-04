bf9a47c270798f074310f2c82c5ee8697ea4224b
Rizkyputripratiwi <99009780+Rizkyputripratiwi@users.noreply.github.com>
2024-03-04 22:34:25 +0700

    Submission Data Analyst

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
-Run Streamlit app
+# Analisis Data Publik E-Commerce dengan Python - Dicoding
 
+## [Klik di sini untuk melihat aplikasi](https://submission-esz6hbbjvziqngxrazzyk7.streamlit.app/)
+
+
+
+## Struktur Proyek
+
+- `dashboard/`: Direktori ini berisi dashboard.py yang digunakan untuk membuat dashboard hasil analisis data.
+- `data/`: Direktori yang berisi file data CSV mentah.
+- `Proyek_Analisis_Data.ipynb`: File ini digunakan untuk melakukan analisis data.
+- `README.md`: File dokumentasi ini.
+
+## Bagaimana Cara Instalasi?
+
+1. Clone repositori ini ke komputer lokal Anda:
+
+```
+git clone https://github.com/Maretaaliana/data-analyst-dicoding.git
+```
+
+2. Buka direktori proyek
+
+```
+cd submission
+```
+
+3. Instal paket Python yang diperlukan dengan menjalankan perintah:
+
+```
+pip install -r requirements.txt
+```
+
+## Penggunaan
+
+1. **Pemrosesan Data**: Script pemrosesan data tersedia dalam file `Proyek_Analisis_Data.ipynb` untuk mempersiapkan dan membersihkan data.
+
+2. **Analisis Data Eksploratif (EDA)**: Jelajahi dan analisis data menggunakan script Python yang tersedia. Temuan EDA dapat membantu memahami pola data publik e-commerce.
+
+3. **Visualisasi**: Jalankan dashboard Streamlit untuk eksplorasi data interaktif:
+
+```
+cd submission/dashboard
 streamlit run dashboard.py
+```
+
+Akses dashboard di browser web Anda di `http://localhost:8501`.
+
+## Sumber Data
+
+Proyek ini menggunakan Dataset Publik E-Commerce dari [Proyek Akhir Belajar Analisis Data dengan Python](https://drive.google.com/file/d/1MsAjPM7oKtVfJL_wRp1qmCajtSG1mdcK/view) yang ditawarkan oleh [Dicoding](https://www.dicoding.com/).
--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -13,17 +13,24 @@ for column in date_columns:
 
 
 df["delivery_time"] = (df["order_delivered_customer_date"] - df["order_purchase_timestamp"]).dt.days
-
-
 df["is_late"] = df["delivery_time"] > (df["order_estimated_delivery_date"] - df["order_purchase_timestamp"]).dt.days
 
 
+rfm_df = df.groupby('customer_id').agg({
+    'order_purchase_timestamp': lambda x: (pd.Timestamp.now() - x.max()).days,  
+    'order_id': 'count', 
+    'price': 'sum'  
+}).rename(columns={
+    'order_purchase_timestamp': 'Recency',
+    'order_id': 'Frequency',
+    'price': 'Monetary'
+})
+
+
 st.sidebar.title("Filter Waktu")
-min_date = df["order_purchase_timestamp"].min().to_pydatetime().date()
-max_date = df["order_purchase_timestamp"].max().to_pydatetime().date()
+min_date = df["order_purchase_timestamp"].min().date()
+max_date = df["order_purchase_timestamp"].max().date()
 start_date, end_date = st.sidebar.date_input("Rentang Waktu", min_value=min_date, max_value=max_date, value=(min_date, max_date))
-
-
 start_date = pd.to_datetime(start_date)
 end_date = pd.to_datetime(end_date)
 
@@ -31,16 +38,13 @@ end_date = pd.to_datetime(end_date)
 df_filtered = df[(df["order_purchase_timestamp"] >= start_date) & (df["order_purchase_timestamp"] <= end_date)]
 
 
-average_delivery_time = df_filtered.groupby("order_status")["delivery_time"].mean().reset_index()
-
-
-late_orders_by_seller = df_filtered[df_filtered["is_late"]].groupby("seller_id")["order_id"].count().reset_index()
-
-
-st.title("Analisis Pengiriman")
+st.header("Dashboard Analisis Pengiriman")
 
 
 st.subheader("Waktu Pengiriman Rata-Rata Berdasarkan Status Pesanan")
+
+
+average_delivery_time = df_filtered.groupby("order_status")["delivery_time"].mean().reset_index()
 fig, ax = plt.subplots()
 sns.barplot(data=average_delivery_time, x="order_status", y="delivery_time", ax=ax)
 ax.set_xlabel("Status Pesanan")
@@ -48,3 +52,37 @@ ax.set_ylabel("Waktu Pengiriman Rata-Rata (hari)")
 ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
 st.pyplot(fig)
 
+
+st.header("Analisis Pola Waktu Pesanan")
+
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.hist(df_filtered['order_purchase_timestamp'], bins=30, color='pink', edgecolor='black')
+ax.set_title('Distribusi Waktu Pembelian Pesanan')
+ax.set_xlabel('Waktu')
+ax.set_ylabel('Frekuensi')
+st.pyplot(fig)
+
+
+st.subheader("Analisis RFM")
+
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20, 10))
+sns.barplot(y=rfm_df.sort_values(by="Recency").head(5).index, x="Recency", data=rfm_df.sort_values(by="Recency").head(5))
+plt.title("Top 5 Customers by Recency", fontsize=50)
+plt.ylabel("Customer ID", fontsize=50)
+st.pyplot(fig)
+
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20, 10))
+sns.barplot(y=rfm_df.sort_values(by="Frequency", ascending=False).head(5).index, x="Frequency", data=rfm_df.sort_values(by="Frequency", ascending=False).head(5))
+plt.title("Top 5 Customers by Frequency", fontsize=50)
+plt.ylabel("Customer ID", fontsize=50)
+st.pyplot(fig)
+
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20, 10))
+sns.barplot(y=rfm_df.sort_values(by="Monetary", ascending=False).head(5).index, x="Monetary", data=rfm_df.sort_values(by="Monetary", ascending=False).head(5))
+plt.title("Top 5 Customers by Monetary", fontsize=50)
+plt.ylabel("Customer ID", fontsize=50)
+st.pyplot(fig)
